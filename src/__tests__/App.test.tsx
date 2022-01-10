b981d331ba13d024ca5aa93747224a4cf2a753bd
Alex <alex-remedy@MacBook-Pro-Alex.local>
2022-01-10 13:27:06 +0300

    Set up store and weather API

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,17 +1,39 @@
 import {render} from '@testing-library/react-native';
 import React from 'react';
+import {Provider} from 'react-redux';
 import {View} from 'react-native';
 import App from '../App';
 import AppNavigator from '../screens';
+import store from '../store';
 
 jest.mock('../screens', () => jest.fn());
+jest.mock('react-redux', () => {
+  return {
+    ...jest.requireActual<object>('react-redux'),
+    Provider: jest.fn(),
+  };
+});
 
 describe('App', () => {
   test('Should render routes', () => {
+    (Provider as jest.Mock).mockImplementationOnce(({children}) => children);
+
     (AppNavigator as jest.Mock).mockReturnValueOnce(
       <View testID="mock-routes" />,
     );
     const wrapper = render(<App />);
     wrapper.getByTestId('mock-routes');
+  });
+
+  test('Should render Provider', () => {
+    let providerStore!: typeof store;
+
+    (Provider as jest.Mock).mockImplementationOnce(({store}) => {
+      providerStore = store;
+      return <View testID="mock-provider" />;
+    });
+
+    const wrapper = render(<App />);
+    wrapper.getByTestId('mock-provider');
   });
 });
